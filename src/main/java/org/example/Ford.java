46811322546e2acd3a901ba257ef9b2331fc42a9
Atilla-Koz <atillakoz@hotmail.com>
2024-05-29 22:39:44 +0300

    Ford and holden done

--- src/main/java/org/example/Ford.java
+++ src/main/java/org/example/Ford.java
@@ -1,4 +1,23 @@
 package org.example;
 
 public class Ford extends  Car{
+
+    public Ford(int cylinders, String name) {
+        super(cylinders, name);
+    }
+
+    @Override
+    public String startEngine() {
+        return getClass().getSimpleName() + " engine is starting";
+    }
+
+    @Override
+    public String accelerate() {
+        return getClass().getSimpleName() + " is accelerating rapidly";
+    }
+
+    @Override
+    public String brake() {
+        return getClass().getSimpleName() + " is braking efficiently";
+    }
 }
--- src/main/java/org/example/Holden.java
+++ src/main/java/org/example/Holden.java
@@ -1,4 +1,23 @@
 package org.example;
 
 public class Holden extends Car {
+
+    public Holden(int cylinders, String name) {
+        super(cylinders, name);
+    }
+
+    @Override
+    public String startEngine() {
+        return getClass().getSimpleName() + " engine is starting";
+    }
+
+    @Override
+    public String accelerate() {
+        return getClass().getSimpleName() + " is accelerating powerfully";
+    }
+
+    @Override
+    public String brake() {
+        return getClass().getSimpleName() + " is braking effectively";
+    }
 }
